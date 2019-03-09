dd8bf40db39949909277bc63cbbadf7f8489410d
LEO <asda2753596@gmail.com>
2019-03-09 13:51:51 +0800

    add range

--- import.py
+++ import.py
@@ -1,6 +1,9 @@
 import random
-r = random.randint(1, 100)
-print(r)
+start = input('請決定開始決定值範圍:')
+end = input('請決定開始決定值範圍:')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:
 	count = count + 1
